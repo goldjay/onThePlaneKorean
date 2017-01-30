bb96197840574f4ae9ac800a1cc69730b10ba45e
goldjay <jay.steingold@gmail.com>
2017-01-30 10:37:09 +0900

    Added view controllers for levels and segue

---
+++ onThePlaneKorean/TableViewController.swift
@@ -0,0 +1,120 @@
+//
+//  TableViewController.swift
+//  onThePlaneKorean
+//
+//  Created by Jay Steingold on 1/30/17.
+//  Copyright © 2017 Goldjay. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let levels = 9
+    var currentDeck: [[String]] = [[]]
+    //Default value for decks
+    var decks = [Deck]()
+    
+    var sentFromDetail: Deck?
+    var numFromDetail: Int?
+    
+    var highScore: [Int] = [] //Saved data for highScore
+    var completed: [Bool] = [] //Saved Data for if a deck is completed
+    
+    //Other possible stats
+        //Average time
+        //Mastery of individual words
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Now in table view!")
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return levels
+    }
+    
+    override func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "level", for: indexPath)
+        cell.textLabel?.text = "Level " + String(indexPath.row + 1)
+        cell.tag = indexPath.row + 1
+        
+        cell.backgroundColor = UIColor.black
+        return cell
+        
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+     */
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
