3ea1e335a0ff6166c8db5abd305e6aa67809e002
goldjay <jay.steingold@gmail.com>
2017-02-05 09:18:46 +0900

    Added timer to quiz

--- onThePlaneKorean/QuestionViewController.swift
+++ onThePlaneKorean/QuestionViewController.swift
@@ -20,11 +20,14 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var timerBar: UIProgressView!
     
+    //From menu view
     var num: Int = 0
     var deck: [[String]] = []
     var sendBack: sendBack?
     
+    //Data from the quiz
     var correctAnswer: Int = 0
     var numAnswered: Int = 0
     var numCorrect: Int = 0
@@ -33,65 +36,47 @@ class QuestionViewController: UIViewController {
     var completed: Bool = false
     var highScore: Double = 0
     
+    //For the timer
+    var timer = Timer()
+    var poseDuration = 40
+    var indexProgressBar = 0
+    var currentPoseIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         button1.layer.cornerRadius = 10 //For rounding the corners
         button2.layer.cornerRadius = 10
         button3.layer.cornerRadius = 10
-
-        //print(num)
-        //print(deck)
+        
+        timerBar.progress = 0.0
         
         askQuestion()
-        
-        // Do any additional setup after loading the view.
     }
 
     func askQuestion(action: UIAlertAction! = nil) {
-        //Shuffle cards in the deck
-        //Shuffle Q and A's (Maybe move to detailView)
         
-        //Possibly change to not shuffle every time
-        
-        //CHANGE TO SHUFFLE ONCE YOU MOVED THROUGH THE DECK
-        let shuffledDeck = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: deck) as! [[String]]
-        
-        //Choose a random answer
-        correctAnswer = Int(arc4random_uniform(3))
-        //print("CORRECT ANSWER IN askQuestion: ")
-        //print(correctAnswer)
-        
-        
-        button1.setTitle(shuffledDeck[0][1], for: UIControlState.normal)
-        button2.setTitle(shuffledDeck[1][1], for: UIControlState.normal)
-        button3.setTitle(shuffledDeck[2][1], for: UIControlState.normal)
-        
-        questionLabel.text = shuffledDeck[correctAnswer][0]
-    }
-    
-    func backToMenu(action: UIAlertAction! = nil) {
-        //Send info back
-        //Send info back
-        //sendBack?.setSentData(num: num, highScore: highScore, completed: completed)
-        navigationController!.pushViewController(storyboard!.instantiateViewController(withIdentifier: "Menu") as UIViewController, animated: true)
-
-    }
-    
-    @IBAction func answerTapped(_ sender: UIButton) {
-        
-        //If the title on the button tapped is the same as the correctAnswer
-        if sender.tag == correctAnswer {
-            //print("YOU GOT ONE CORRECT")
-            numCorrect += 1
-            //print("NUMBER OF CORRECT ANSWERS:")
-            //print(numCorrect)
-        }
-        
-        numAnswered += 1
-        //If have have answered enough questions (CHANGED FOR DEBUG)
-        if numAnswered == 1 {
+        if(!checkIfFinished()){
+            //TO DO: change shuffle to happen after user reaches end of deck
+            let shuffledDeck = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: deck) as! [[String]]
             
+            //Choose a random answer
+            correctAnswer = Int(arc4random_uniform(3))
+            
+            button1.setTitle(shuffledDeck[0][1], for: UIControlState.normal)
+            button2.setTitle(shuffledDeck[1][1], for: UIControlState.normal)
+            button3.setTitle(shuffledDeck[2][1], for: UIControlState.normal)
+            
+            questionLabel.text = shuffledDeck[correctAnswer][0]
+            
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(QuestionViewController.setProgressBar), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func checkIfFinished() -> Bool{
+        if numAnswered == 3 {
+            
+            timer.invalidate()
             print(Double(numCorrect/numAnswered))
             
             //If you did well enough, you can move to the next level
@@ -111,24 +96,76 @@ class QuestionViewController: UIViewController {
             
             //Send info back
             //sendBack?.setSentData(num: num, highScore: highScore, completed: completed)
-            //print("HighScore: \(highScore)")
-            //print("Completed: \(completed)")
-            
             
             //Reset stats
             numAnswered = 0
             numCorrect = 0
             
+            
+            //TO DO: Change to transition to view instead
             let ac = UIAlertController(title: nil, message: message, preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "Back", style: .default, handler: backToMenu))
             ac.addAction(UIAlertAction(title: "Again", style: .default, handler: askQuestion))
             
             present(ac, animated: true)
-            
+            return true
         }
-        else{
-                askQuestion()
+        return false
+    }
+    
+    //MARK: timer
+    func setProgressBar()
+    {
+        if indexProgressBar == poseDuration
+        {
+            numAnswered += 1
+            resetTimer()
+            askQuestion()
         }
+        
+        // update the display
+        // use poseDuration - 1 so that you display 20 steps of the the progress bar, from 0...19
+        timerBar.progress = Float(indexProgressBar) / Float(poseDuration - 1)
+        
+        // increment the counter
+        indexProgressBar += 1
+    }
+    
+    func getNextPoseData()
+    {
+        // do next pose stuff
+        currentPoseIndex += 1
+        print(currentPoseIndex)
+    }
+    
+    func resetTimer() {
+        timer.invalidate()
+        indexProgressBar = 0
+        currentPoseIndex = 0
+        
+    }
+    
+    func backToMenu(action: UIAlertAction! = nil) {
+        //Send info back
+        //Send info back
+        //sendBack?.setSentData(num: num, highScore: highScore, completed: completed)
+        navigationController!.pushViewController(storyboard!.instantiateViewController(withIdentifier: "Menu") as UIViewController, animated: true)
+
+    }
+    
+    @IBAction func answerTapped(_ sender: UIButton) {
+        
+        resetTimer()
+
+        //If the title on the button tapped is the same as the correctAnswer
+        if sender.tag == correctAnswer {
+            numCorrect += 1
+        }
+        
+        numAnswered += 1
+        //If have have answered enough questions (CHANGED FOR DEBUG)
+        
+        askQuestion()
     }
 
 }
